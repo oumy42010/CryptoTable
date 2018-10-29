2261e135b6bdd8d3ece5ea574db0876a98fe1ac3
oumy42010 <diatta.oumy@yahoo.com>
2018-10-29 21:58:39 +0100

    Ajout de la dominance du bitcoin et du paragraphe

--- marketcap.js
+++ marketcap.js
@@ -26,7 +26,19 @@ function populateHeader(jsonObj) {
 	myH1Elt.style.margin = "4px";
 	myH1Elt.innerHTML = "<p style='color:#B09F91;'> Market cap : <a href='https://coinmarketcap.com/charts' style='text-decoration: none; color:#375D81;'>$" + jsonObj['total_market_cap_usd'].toLocaleString() + "</a></p>";
 
+	
+	var myBtcDominanceElt = document.createElement('h2');
+	myBtcDominanceElt.style.color = "#375D81";
+	myBtcDominanceElt.style.margin = "15px";
+	myBtcDominanceElt.textContent = '24h Vol: $' + jsonObj['total_24h_volume_usd'].toLocaleString()  + ' / BTC Dominance: ' + jsonObj['bitcoin_percentage_of_market_cap'] + '%';
+
+	var myPElt = document.createElement('p');
+	myPElt.style.color = "#375D81";
+	myPElt.style.margin = "15px";
+	myPElt.textContent = "This site lists the top 50 cryptocurrencies and offers you a simple way to compare performance over the past week."
 			
 	header.appendChild(myH1Elt);  
 	header.appendChild(myTimeElt);
+	header.appendChild(myBtcDominanceElt);
+	header.appendChild(myPElt);
 }
